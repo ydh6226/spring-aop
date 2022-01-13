d8207860a0e03d87ba8520b74bd0423b8a9fb72a
ydh6226 <ydh6226@naver.com>
2022-01-14 00:29:22 +0900

    다양한 어드바이스 적용

--- src/main/java/com/aop/AopApplication.java
+++ src/main/java/com/aop/AopApplication.java
@@ -1,14 +1,12 @@
 package com.aop;
 
-import com.aop.order.aop.AspectV1;
-import com.aop.order.aop.AspectV3;
-import com.aop.order.aop.AspectV4Pointcut;
-import com.aop.order.aop.AspectV5Order;
+import com.aop.order.aop.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+@Import(AspectV6Advice.class)
 @SpringBootApplication
 public class AopApplication {
 
