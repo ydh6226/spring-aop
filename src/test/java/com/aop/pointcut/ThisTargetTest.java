25170c53be189ea56cfb49fc6cefc05eac5a4f79
ydh6226 <ydh6226@naver.com>
2022-01-17 22:18:46 +0900

    this, target 예제

---
+++ src/test/java/com/aop/pointcut/ThisTargetTest.java
@@ -0,0 +1,63 @@
+package com.aop.pointcut;
+
+import com.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+/**
+ * spring.aop.proxy-target-class=true(default) 인터페이스, 구체클래스 상관 없이 CGLIB
+ * spring.aop.proxy-target-class=false 인터페이스 있으면: JDK 동적 프록시, 없으면: CGLIB
+ */
+@Slf4j
+@Import(ThisTargetTest.ThisTargetAspect.class)
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=false") //JDK 동적 프록시
+@SpringBootTest(properties = "spring.aop.proxy-target-class=true") //JDK 동적 프록시
+public class ThisTargetTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy: {}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class ThisTargetAspect {
+
+        // 부모 타입 허용
+        @Around("this(com.aop.member.MemberService)")
+        public Object doThisInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        // 부모 타입 허용
+        @Around("target(com.aop.member.MemberService)")
+        public Object doTargetInterface(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-interface] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("this(com.aop.member.MemberServiceImpl)")
+        public Object doThis(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[this-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+        @Around("target(com.aop.member.MemberServiceImpl)")
+        public Object doTarget(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[target-impl] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+
+}
